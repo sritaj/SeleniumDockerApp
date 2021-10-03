1871c233ba4e3cce0bff5583364f393691f31528
Sritaj <sritajp@gmail.com>
2021-10-03 23:30:55 +0530

    Added enums for WaitStrategy and ConfigProperties

---
+++ src/main/java/enums/ConfigProperties.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum ConfigProperties {
+
+    LOCALRUN,
+    URL,
+
+}
---
+++ src/main/java/enums/WaitStrategy.java
@@ -0,0 +1,10 @@
+package enums;
+
+public enum WaitStrategy {
+
+    VISIBILITY,
+    PRESENCE,
+    CLICKABLE,
+    NONE;
+
+}
