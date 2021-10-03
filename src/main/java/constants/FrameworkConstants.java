b6ebd3f857d16b4988d8e1c782b1e03706f0531d
Sritaj <sritajp@gmail.com>
2021-10-03 23:31:49 +0530

    Updated waitHelper method based on Enums

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -8,6 +8,7 @@ public final class FrameworkConstants {
     private static final String RESOURCEPATH = System.getProperty("user.dir");
     private static final String CHROMEDRIVERPATH = RESOURCEPATH + "/src/main/resources/drivers/chromedriver";
     private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/main/resources/config/config.properties";
+    private static final int EXPLICITWAIT_TIMEOUT = 10;
 
     public static String getChromeDriverPath() {
         return CHROMEDRIVERPATH;
@@ -15,5 +16,9 @@ public final class FrameworkConstants {
 
     public static String getPropertiesFilePath() {
         return PROPERTIESFILEPATH;
+    }
+
+    public static int getExplicitWaitTimeOut() {
+        return EXPLICITWAIT_TIMEOUT;
     }
 }
--- src/main/java/helper/WaitHelper.java
+++ src/main/java/helper/WaitHelper.java
@@ -1,6 +1,9 @@
 package helper;
 
+import constants.FrameworkConstants;
 import driver.DriverManager;
+import enums.WaitStrategy;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,12 +12,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public final class WaitHelper {
 
     private WebDriver driver;
+    private ElementHelper elementHelper;
 
     public WaitHelper() {
         this.driver = DriverManager.getDriver();
+        this.elementHelper = new ElementHelper();
     }
 
-    public void waitForElementVisibility(int waitTime, WebElement element) {
-        new WebDriverWait(driver, waitTime).until(ExpectedConditions.visibilityOf(element));
+    public WebElement performExplicitWait(WaitStrategy waitType, By locator) {
+        WebElement element = null;
+        if(waitType == WaitStrategy.VISIBILITY){
+            element = new WebDriverWait(driver, FrameworkConstants.getExplicitWaitTimeOut()).until(ExpectedConditions.visibilityOfElementLocated(locator));
+        }else if(waitType == WaitStrategy.PRESENCE){
+            element = new WebDriverWait(driver, FrameworkConstants.getExplicitWaitTimeOut()).until(ExpectedConditions.presenceOfElementLocated(locator));
+        }else if(waitType == WaitStrategy.CLICKABLE){
+            element = new WebDriverWait(driver, FrameworkConstants.getExplicitWaitTimeOut()).until(ExpectedConditions.elementToBeClickable(locator));
+        }else if(waitType == WaitStrategy.NONE){
+            return elementHelper.getElement(locator);
+        }
+        return element;
     }
 }
