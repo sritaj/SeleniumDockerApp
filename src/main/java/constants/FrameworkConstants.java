53c97bf67edd811bf4682fab87e62ae4564c16d3
Sritaj <sritajp@gmail.com>
2021-10-03 21:36:59 +0530

    Added constants for chromedriver and properties file reference

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -2,17 +2,18 @@ package constants;
 
 public final class FrameworkConstants {
 
-    private FrameworkConstants(){}
+    private FrameworkConstants() {
+    }
 
-    private static final String RESOURCEPATH = System.getProperty("user.dir") ;
-    private static final String CHROMEDRIVERPATH =  RESOURCEPATH + "/src/main/resources/drivers/chromedriver";
+    private static final String RESOURCEPATH = System.getProperty("user.dir");
+    private static final String CHROMEDRIVERPATH = RESOURCEPATH + "/src/main/resources/drivers/chromedriver";
     private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/main/resources/config/config.properties";
 
-    public static String getChromeDriverPath(){
+    public static String getChromeDriverPath() {
         return CHROMEDRIVERPATH;
     }
 
-    public static String getPropertiesFilePath(){
+    public static String getPropertiesFilePath() {
         return PROPERTIESFILEPATH;
     }
 }
