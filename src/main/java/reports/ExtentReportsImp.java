0b74e2ac4558f0d1f180335471b0b32ae2bcafa3
Sritaj <sritajp@gmail.com>
2021-10-05 01:10:38 +0530

    Updated ExtentReportsImp with path from constants

--- src/main/java/reports/ExtentReportsImp.java
+++ src/main/java/reports/ExtentReportsImp.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import constants.FrameworkConstants;
 import utilities.TakeScreenshotImp;
 
 import java.util.Objects;
@@ -21,7 +22,7 @@ public final class ExtentReportsImp {
     public static void initializeReport() {
         if (Objects.isNull(extent)) {
             extent = new ExtentReports();
-            spark = new ExtentSparkReporter("target/reports/AutomationReport.html");
+            spark = new ExtentSparkReporter(FrameworkConstants.getExtentReportPath() + "AutomationReport.html");
             spark.config().setTheme(Theme.DARK);
             spark.config().setDocumentTitle("AutomationReport");
             spark.config().setReportName("Selenium Docker App");
