16137dc54a6e9c24ced46caf627e25690c6365f3
Sritaj <sritajp@gmail.com>
2021-10-04 23:56:08 +0530

    Added Extent logger for clicking

--- src/main/java/helper/ElementHelper.java
+++ src/main/java/helper/ElementHelper.java
@@ -4,6 +4,7 @@ import driver.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import reports.ExtentReportsImp;
 
 public final class ElementHelper {
 
@@ -19,6 +20,10 @@ public final class ElementHelper {
 
     public void clickElement(By locator) {
         getElement(locator).click();
+        if(getElement(locator).getAttribute("name")!=null){
+            ExtentReportsImp.logSteps("Clicked on '"+getElement(locator).getAttribute("name")+ "'");
+        }
+
     }
 
     public void sendKeysToElement(By locator, String input) {
