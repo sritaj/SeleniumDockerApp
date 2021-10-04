ea00932ff4903024136a14e6739dacd40caac630
Sritaj <sritajp@gmail.com>
2021-10-05 01:08:32 +0530

    Added the path for ExtentReports and created Getter method to save files based on config value

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -1,5 +1,11 @@
 package constants;
 
+import enums.ConfigProperties;
+import utilities.PropertiesFileImp;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public final class FrameworkConstants {
 
     private FrameworkConstants() {
@@ -8,6 +14,7 @@ public final class FrameworkConstants {
     private static final String RESOURCEPATH = System.getProperty("user.dir");
     private static final String CHROMEDRIVERPATH = RESOURCEPATH + "/src/main/resources/drivers/chromedriver";
     private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/main/resources/config/config.properties";
+    private static final String EXTENTREPORTSPATH = RESOURCEPATH + "/target/reports/";
     private static final int EXPLICITWAIT_TIMEOUT = 10;
 
     public static String getChromeDriverPath() {
@@ -20,5 +27,12 @@ public final class FrameworkConstants {
 
     public static int getExplicitWaitTimeOut() {
         return EXPLICITWAIT_TIMEOUT;
+    }
+
+    public static String getExtentReportPath() {
+        if (PropertiesFileImp.getDataFromPropertyFile(ConfigProperties.OVERRIDEREPORTS).equalsIgnoreCase("yes")) {
+            return EXTENTREPORTSPATH;
+        }
+        return EXTENTREPORTSPATH + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + "/";
     }
 }
